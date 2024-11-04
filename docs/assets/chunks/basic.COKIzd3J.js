26f76ed93d308e83f27e5ed8a42bcccae1e48160
Orange <kcj65197@gmail.com>
2024-11-04 16:40:59 +0800

    init

---
+++ docs/assets/chunks/basic.COKIzd3J.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./basic.vue_vue_type_script_setup_true_lang.GWBA1RKC.js";import"./framework.DEdMHMZ3.js";export{m as default};
---
+++ docs/assets/chunks/basic.vue_vue_type_script_setup_true_lang.GWBA1RKC.js
@@ -0,0 +1,1 @@
+import{d as s,p as a,B as r,o as p,c,a as i,G as l}from"./framework.DEdMHMZ3.js";const x=s({__name:"basic",setup(f){const n=a("");return(m,e)=>{const t=r("MyAffix");return p(),c("div",null,[e[1]||(e[1]=i(" this is basic myAffix example. ")),l(t,{content:n.value,onUpdate:e[0]||(e[0]=o=>n.value=o)},null,8,["content"])])}}});export{x as _};
